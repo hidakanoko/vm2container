66cc06438df2de7e59edf8cd98d18ce36152e655
takahashi <hidakanoko.k7@gmail.com>
2019-01-03 19:09:44 +0900

    Add stretch_parent_path method to stretch path
     - Modify to fix file path to realpath except last element of path
     - Add tests

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+import platform
 import shutil
 import tempfile
 import unittest
+from nose.tools import ok_, eq_, raises
 
 from v2c.archive import stretch_parent_path
 
@@ -11,22 +13,40 @@ from v2c.archive import stretch_parent_path
 class TestArchive(unittest.TestCase):
 
     def setUp(self):
+        # check platform
+        if platform.system() != 'Linux':
+            raise SystemError('Unable to run tests except Linux environment')
         self.tmp_dir = tempfile.mkdtemp()
 
     def tearDown(self):
         if self.tmp_dir is not None and os.path.exists(self.tmp_dir):
             shutil.rmtree(self.tmp_dir)
 
-    def test__stretch_parent_path__return_None_given_path_is_None(self):
+    @raises(OSError)
+    def test_stretch_parent_path__throws_FileNotFoundError_given_path_NOT_exist(self):
         # given
-        path = None
+        path = os.path.join(self.tmp_dir, 'dir_not_exist')
 
         # when, then
-        self.assertIsNone(stretch_parent_path(path))
+        stretch_parent_path(path)
 
-    def test__stretch_parent_path__return_none_given_path_is_Empty(self):
+    def test_stretch_parent_path__return_root_given_root(self):
         # given
-        path = ''
+        path = '/'
 
         # when, then
-        self.assertIsNone(stretch_parent_path(path))
+        eq_(stretch_parent_path(path), '/')
+
+    def test_stretch_parent_path__return_realpath_given_parent_dir_is_symlink(self):
+        # given
+        link_target_dir = tempfile.mkdtemp(dir=self.tmp_dir)
+
+        slink = os.path.join(self.tmp_dir, 'slink')
+        os.symlink(link_target_dir, slink)
+
+        (file_handler, file_under_symlink_dir) = tempfile.mkstemp(dir=slink)
+        path_filename = os.path.basename(file_under_symlink_dir)
+        realpath = os.path.join(link_target_dir, path_filename)
+
+        # when, then
+        eq_(stretch_parent_path(file_under_symlink_dir), realpath)
--- v2c/archive.py
+++ v2c/archive.py
@@ -14,8 +14,10 @@ class ArchiveHandler:
     def __init__(self):
         self.logger = ConsoleLogger.get_instance()
         self._cmd_tar = CommandExecutor.find_tool('tar')
+        self._output = None
 
     def create_archive(self, files, dest='/tmp/archive.tgz'):
+        self._output = None
         t = PrintProgressThread()
         t.set_start_message('Creating archive...')
         try:
@@ -26,13 +28,15 @@ class ArchiveHandler:
         finally:
             t.stop()
             t.join(20)
+        if self._output is not None:
+            self.logger.info(self._output)
         if os.path.exists(dest):
             self.logger.info('Archive created in ' + dest)
 
     def _create_archive_filelist(self, files, filelist):
         for f in files:
             if os.path.exists(f):
-                filelist.write(f + '\n')
+                filelist.write(stretch_parent_path(f) + '\n')
 
     def _create_archive(self, filelist_path, dest, gzip=True):
         cmd = self._cmd_tar + ' c'
@@ -40,12 +44,20 @@ class ArchiveHandler:
             cmd+= 'z'
         cmd += 'f ' + dest + ' -T '\
                 + filelist_path\
-                + ' --no-recursion'
+                + ' --no-recursion'\
+                + ' 2&>1'
         try:
-            CommandExecutor.execute(cmd)
+            self._output = CommandExecutor.execute(cmd)
         except subprocess.CalledProcessError as e:
-            self.logger.warn('Archive creation failed with status code ' + str(e.returncode))
+            self.logger.warn('Archive creation failed with status code ' + str(e.returncode)
+                             + ', ' + str(e.output))
 
 
 def stretch_parent_path(path):
-    return path
+    if not os.path.exists(path):
+        raise OSError(path)
+    if str(path).strip == '/':
+        return path
+    f_name = os.path.basename(path)
+    parent_dir = path[:path.rfind(f_name)]
+    return os.path.join(os.path.realpath(parent_dir), f_name)
