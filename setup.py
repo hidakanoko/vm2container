f76dfb1ba96b4e70bc4eec4ab148021675f4e484
takahashi <hidakanoko.k7@gmail.com>
2019-01-03 12:10:49 +0900

    Modify project structure
     - thanks to https://github.com/kennethreitz/samplemod

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='v2c',
+    version='0.1.0',
+    description='Create container archive from running Linux machine',
+    long_description=readme,
+    author='Kazuhiro Takahashi',
+    author_email='hidakanoko.k7@gmail.com',
+    url='https://github.com/hidakanoko/vm2container',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
