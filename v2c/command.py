9e02a58220468f6070c144a6a95043492ada9cbb
takahashi <hidakanoko.k7@gmail.com>
2019-01-14 16:19:30 +0900

    fix to use built in logger class #3
     - Modify "message" package to use built-in logger class
     - Remove Classes and convert class methods to package methods.

--- v2c/command.py
+++ v2c/command.py
@@ -4,40 +4,33 @@ import os
 import subprocess
 
 
-class CommandExecutor:
+def execute(cmd):
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
+    try:
+        out, err = proc.communicate()
+    except TimeoutError:
+        proc.kill()
+        out, err = proc.communicate()
+    return_code = proc.poll()
+    if return_code != 0:
+        raise CommandExecuteError(return_code, cmd, stderr=err, output=out)
+    if out is None:
+        out = ''
+    return out.strip()
 
-    _cmd_which = None
 
-    @staticmethod
-    def execute(cmd):
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
-        try:
-            out, err = proc.communicate()
-        except TimeoutError:
-            proc.kill()
-            out, err = proc.communicate()
-        return_code = proc.poll()
-        if return_code != 0:
-            raise CommandExecuteError(return_code, cmd, stderr=err, output=out)
-        if out is None:
-            out = ''
-        return out.strip()
+def find_tool(tool_name):
+    detect_which()
+    return execute(detect_which() + " " + tool_name)
 
-    @staticmethod
-    def find_tool(tool_name):
-        CommandExecutor._detect_which()
-        return CommandExecutor.execute(CommandExecutor._cmd_which + " " + tool_name)
 
-    @staticmethod
-    def _detect_which():
-        if CommandExecutor._cmd_which is not None:
-            return CommandExecutor._cmd_which
-        cmd_which = "/usr/bin/which"
+def detect_which():
+    cmd_which = "/usr/bin/which"
+    if not os.path.exists(cmd_which):
+        cmd_which = "/bin/which"
         if not os.path.exists(cmd_which):
-            cmd_which = "/bin/which"
-            if not os.path.exists(cmd_which):
-                raise FileNotFoundError("ERROR: which command not found!")
-        CommandExecutor._cmd_which = cmd_which
+            raise FileNotFoundError("ERROR: which command not found!")
+    return cmd_which
 
 
 class CommandExecuteError(subprocess.CalledProcessError):
